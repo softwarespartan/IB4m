bf3d4dfdcba17df54fcdc857bd9479e99b110087
Abel Brown <abelbrown@Abels-MBP.fios-router.home>
2017-01-16 12:32:24 -0500

    "updates for error events"

--- +TWS/Handler.m
+++ +TWS/Handler.m
@@ -19,6 +19,7 @@ classdef Handler < handle
     
     % event buffers
     properties(GetAccess = 'public', SetAccess = 'private')
+                 errorEventBuf;
               orderExeEventBuf;
             openOrdersEventBuf;
            orderStatusEventBuf;
@@ -29,6 +30,7 @@ classdef Handler < handle
     
     % listener handles
     properties(Access = 'private')
+                 errorEventHandle;
               orderExeEventHandle;
             openOrdersEventHandle;
            orderStatusEventHandle;
@@ -49,6 +51,26 @@ classdef Handler < handle
             % init logger
             this.logger = TWS.Logger.getInstance(class(this));
             
+            % get session
+            this.session = TWS.Session.getInstance();
+            
+            % if not connected, do not initialize
+            if ~this.session.eClientSocket.isConnected()
+                
+                % blab about it on the logger
+                this.logger.error([TWS.Logger.this,'>', ' not connected to TWS -- initialization aborted']);
+                
+                % make an error of it
+                error(' not connected to TWS -- initialization aborted');         
+            end
+            
+            % init error listener
+            this.errorEventHandle = event.listener(              ...
+                TWS.Events.getInstance                          ,...
+                TWS.Events.ERROR                                ,...
+                @(s,e)processErrorEvent(e.event)                 ...
+            );
+            
             % init next order id instance
             this.nextOrderIdObj = TWS.NextOrderId.getInstance();
         
@@ -60,53 +82,51 @@ classdef Handler < handle
             );
         
             % create listener for the open orders event
-            this.openOrdersEventHandle =  event.listener(  ...
-                TWS.Events.getInstance                    ,...
-                TWS.Events.OPENORDER                      ,...
-                @(s,e)this.processOpenOrdersEvent(e.event) ...
+            this.openOrdersEventHandle =  event.listener(        ...
+                TWS.Events.getInstance                          ,...
+                TWS.Events.OPENORDER                            ,...
+                @(s,e)this.processOpenOrdersEvent(e.event)       ...
             );
         
             % create listener for the order status event
-            this.orderStatusEventHandle =  event.listener(  ...
-                TWS.Events.getInstance                     ,...
-                TWS.Events.ORDERSTATUS                     ,...
-                @(s,e)this.processOrderStatusEvent(e.event) ...
+            this.orderStatusEventHandle =  event.listener(       ...
+                TWS.Events.getInstance                          ,...
+                TWS.Events.ORDERSTATUS                          ,...
+                @(s,e)this.processOrderStatusEvent(e.event)      ...
             );
         
             % create listener for the position events
-            this.positionsEventHandle =  event.listener(    ...
-                TWS.Events.getInstance                     ,...
-                TWS.Events.POSITIONS                       ,...
-                @(s,e)this.processPositionsEvent(e.event)   ...
+            this.positionsEventHandle =  event.listener(         ...
+                TWS.Events.getInstance                          ,...
+                TWS.Events.POSITIONS                            ,...
+                @(s,e)this.processPositionsEvent(e.event)        ...
             );
             
             % create listener for account update events
-            this.accountUpdateEventHandle =  event.listener(  ...
-                TWS.Events.getInstance                       ,...
-                TWS.Events.ACCOUNTUPDATE                     ,...
-                @(s,e)this.processAccountUpdateEvent(e.event) ...
+            this.accountUpdateEventHandle =  event.listener(     ...
+                TWS.Events.getInstance                          ,...
+                TWS.Events.ACCOUNTUPDATE                        ,...
+                @(s,e)this.processAccountUpdateEvent(e.event)    ...
             );
         
             % create listener for account summary events
-            this.accountSummaryEventHandle =  event.listener(  ...
-                TWS.Events.getInstance                        ,...
-                TWS.Events.ACCOUNTSUMMARY                     ,...
-                @(s,e)this.processAccountSummaryEvent(e.event) ...
+            this.accountSummaryEventHandle =  event.listener(    ...
+                TWS.Events.getInstance                          ,...
+                TWS.Events.ACCOUNTSUMMARY                       ,...
+                @(s,e)this.processAccountSummaryEvent(e.event)   ...
             );
         
             % init buffer size
-            bufsz = 128;
+            bufsz = 256;
             
             % init buffers for events
+                     this.errorEventBuf = org.apache.commons.collections.buffer.CircularFifoBuffer(bufsz);
                   this.orderExeEventBuf = org.apache.commons.collections.buffer.CircularFifoBuffer(bufsz);
                 this.openOrdersEventBuf = org.apache.commons.collections.buffer.CircularFifoBuffer(bufsz);
                this.orderStatusEventBuf = org.apache.commons.collections.buffer.CircularFifoBuffer(bufsz);
                  this.positionsEventBuf = org.apache.commons.collections.buffer.CircularFifoBuffer(bufsz);
              this.accountUpdateEventBuf = org.apache.commons.collections.buffer.CircularFifoBuffer(bufsz);
             this.accountSummaryEventBuf = org.apache.commons.collections.buffer.CircularFifoBuffer(bufsz);
-            
-            % get session
-            this.session = TWS.Session.getInstance();
             
             % request account summary details
             this.session.eClientSocket.reqAccountSummary(0,'All',this.attrkeys);  pause(1);
@@ -125,50 +145,57 @@ classdef Handler < handle
     
     methods
         
-        function id = nextOrderId(this)
+        function id = nextOrderId(this)                               
             id = this.nextOrderIdObj.nextOrderId;
         end
         
-        function processExecutionDetailsEvent(this,event)          %#ok<*INUSD>
+        function processErrorEvent(this,event)            %#ok<*INUSD>
+            this.logger.trace([TWS.Logger.this,'>', ' recived error ']);
+            this.errorEventBuf.add(event);
+        end
+        
+        function processExecutionDetailsEvent(this,event) %#ok<*INUSD>
             this.logger.trace([TWS.Logger.this,'>', ' recived execution details']);
             this.orderExeEventBuf.add(event);
         end
         
-        function processOpenOrdersEvent(this,event)
+        function processOpenOrdersEvent(this,event)                   
             this.logger.trace([TWS.Logger.this,'>', ' recived open order event']);
             this.openOrdersEventBuf.add(event);
         end
         
-        function processOrderStatusEvent(this,event)
+        function processOrderStatusEvent(this,event)                  
             this.logger.trace([TWS.Logger.this,'>', ' recived order status event']);
             this.orderStatusEventBuf.add(event);
         end
         
-        function processPositionsEvent(this,event)
+        function processPositionsEvent(this,event)                    
             this.logger.trace([TWS.Logger.this,'>', ' recived positions event']);
             this.positionsEventBuf.add(event);
         end
         
-        function processAccountUpdateEvent(this,event)
+        function processAccountUpdateEvent(this,event)                
             this.logger.trace([TWS.Logger.this,'>', ' recived account update event']);
             this.accountUpdateEventBuf.add(event);
         end
         
-        function processAccountSummaryEvent(this,event)
+        function processAccountSummaryEvent(this,event)               
             this.logger.trace([TWS.Logger.this,'>', ' recived account update event']);
             this.accountSummaryEventBuf.add(event);
         end
         
-        function delete(this)                           
+        function delete(this)                                         
             
-            % blab
+            % blab about it on the logger
             this.logger.trace([TWS.Logger.this,'>', ' removing listener handles']);
             
+            % cancel notification requests in TWS
             this.session.eClientSocket.cancelPositions()
             this.session.eClientSocket.reqAutoOpenOrders(false);
             this.session.eClientSocket.reqAccountUpdates(false,this.accountNumber);
             
             % remove listeners
+            delete(this.errorEventHandle         );
             delete(this.orderExeEventHandle      );
             delete(this.openOrdersEventHandle    );
             delete(this.orderStatusEventHandle   );
